893c18d5b7df37c35df67f73b4e59deea0162aa0
Farhad Seddighi <farhadseddighi.fs@gmail.com>
2025-12-27 11:52:20 +0330

    Update main.py with colored output and detailed logging

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 RED = '\033[91m'
 GREEN = '\033[3;4;32m'
@@ -35,6 +36,19 @@ def analyze_data(df):
     avg_usage = intl_calls['Data_Usage_MB'].mean()
     print(f"{GREEN}Average internet usage for international calls:{END} {avg_usage} MB")
 
+    print("\nDrawing diagram...")
+    usage_summary = df.groupby('Call_Type')['Data_Usage_MB'].sum()
+
+    print(f"usage summary: \n{usage_summary}")
+    plt.figure(figsize=(8, 5))  # تنظیم سایز پنجره
+    usage_summary.plot(kind='bar', color=['skyblue', 'orange', 'green'])
+
+    plt.title('Total Internet Usage by Call Type')  # عنوان نمودار
+    plt.xlabel('Call Type')  # برچسب پایین
+    plt.ylabel('Usage (MB)')  # برچسب کناری
+    plt.grid(axis='y')
+
+    plt.show()
 
 print(f'{RED}--- START PROGRAM ---{END}')
 raw_data = load_data('telecom_data.csv')
