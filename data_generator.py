19a18a760f6ef556b90fa05938e9245c5bc6a81f
Farhad Seddighi <farhadseddighi.fs@gmail.com>
2025-12-28 12:03:18 +0330

    Add Fraud Detection module for high usage/duration

---
+++ data_generator.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import random
+from datetime import datetime, timedelta
+
+RED = '\033[91m'
+GREEN = '\033[3;2;32m'
+ITALIC = '\033[3m'
+END = '\033[0m'
+
+
+def generate_large_dataset(filename, num_records=10000):
+    print(f"\n--- {RED}Starting data generation for {num_records} records ---{END}")
+
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=30)
+
+    date_list = []
+    for _ in range(num_records):
+        random_days = random.randint(0, 30)
+        random_seconds = random.randint(0, 86400)
+        random_date = start_date + timedelta(days=random_days, seconds=random_seconds)
+        date_list.append(random_date)
+
+    types = ['Internal', 'International', 'Roaming', 'Emergency']
+    call_types = random.choices(types, weights=[60, 30, 5, 5], k=num_records)
+    durations = np.random.randint(10, 3600, size=num_records)
+
+    data_usage = []
+    for _ in range(num_records):
+        if random.random() < 0.3:
+            data_usage.append(0.0)
+        else:
+            data_usage.append(round(random.uniform(5, 500), 2))
+
+    df = pd.DataFrame({
+        'Date': date_list,
+        'Duration': durations,
+        'Data_Usage': data_usage,
+        'Call_Type': call_types,
+    })
+
+    random_indices = np.random.choice(df.index, 5, replace=False)
+    df.loc[random_indices, 'Duration'] = -100
+
+    random_indices_null = np.random.choice(df.index, 5, replace=False)
+    df.loc[random_indices_null, 'Data_Usage'] = np.nan
+
+    df.to_csv(filename, index=False)
+    print(f"\nSuccessfully created {GREEN}'{filename}'{END} with {GREEN}{len(df)}{END} records.")
+    print(f"\n{RED}--- Data generation complete ---{END}")
+
+
+if __name__ == "__main__":
+    generate_large_dataset('telecom_data_large.csv', num_records=10000)
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+import time
 
 RED = '\033[91m'
 GREEN = '\033[3;4;32m'
 END = '\033[0m'
 ITALIC = '\033[3m'
+InputFile = "telecom_data_large.csv"
 
 
 def load_data(filename):
@@ -23,8 +26,8 @@ def clean_data(df):
     initial_count = len(df)
     df_clean = df.dropna()
     print(f"Removed {initial_count - len(df_clean)} rows with empty data.")
-    df_negative_zero = df_clean[df_clean['Duration_Seconds'] <= 0]
-    df_clean = df_clean[df_clean['Duration_Seconds'] > 0]
+    df_negative_zero = df_clean[df_clean['Duration'] <= 0]
+    df_clean = df_clean[df_clean['Duration'] > 0]
     print(f"Removed {len(df_negative_zero)} records with negative or zero duration seconds.")
     print(f"\n{ITALIC}Final data ready for analysis: {len(df_clean)} records{END}")
     return df_clean
@@ -33,26 +36,62 @@ def clean_data(df):
 def analyze_data(df):
     print(f"\n{RED}--- FINAL REPORT ---\n{END}")
     intl_calls = df[df['Call_Type'] == 'International']
-    avg_usage = intl_calls['Data_Usage_MB'].mean()
-    print(f"{GREEN}Average internet usage for international calls:{END} {avg_usage} MB")
+    avg_usage = intl_calls['Data_Usage'].mean()
+    print(f"{GREEN}Average internet usage for international calls:{END} {avg_usage:.2f} MB")
 
     print("\nDrawing diagram...")
-    usage_summary = df.groupby('Call_Type')['Data_Usage_MB'].sum()
-
+    usage_summary = df.groupby('Call_Type')['Data_Usage'].sum()
     print(f"usage summary: \n{usage_summary}")
-    plt.figure(figsize=(8, 5))  # تنظیم سایز پنجره
-    usage_summary.plot(kind='bar', color=['skyblue', 'orange', 'green'])
 
-    plt.title('Total Internet Usage by Call Type')  # عنوان نمودار
-    plt.xlabel('Call Type')  # برچسب پایین
-    plt.ylabel('Usage (MB)')  # برچسب کناری
-    plt.grid(axis='y')
+    fig, ax = plt.subplots(figsize=(8, 5))
+    usage_summary.plot(kind='bar', color=['skyblue', 'orange', 'green', 'red'], ax=ax)
+    plt.xticks(rotation=45, ha='right')
 
+    ax.set_title('Total Internet Usage by Call Type')
+    ax.set_xlabel('Call Type')
+    ax.set_ylabel('Usage (MB)')
+    ax.yaxis.set_major_locator(mticker.MultipleLocator(50000))
+
+    ax.grid(axis='y', linestyle='--', alpha=0.6)
+    plt.tight_layout()
     plt.show()
 
-print(f'{RED}--- START PROGRAM ---{END}')
-raw_data = load_data('telecom_data.csv')
 
-if raw_data is not None:
-    clean_dataframe = clean_data(raw_data)
-    analyze_data(clean_dataframe)
+def detect_fraud(df):
+    print(f"\n{RED}--- SECURITY CHECK: FRAUD DETECTION ---{END}")
+    high_duration_limit = 3300  # ثانیه (۵۵ دقیقه)
+    high_data_limit = 450
+
+    suspicious_df = df[(df['Duration'] > high_duration_limit) | (df['Data_Usage'] > high_data_limit)]
+    count = len(suspicious_df)
+
+    if count > 0:
+        print(f"{RED}⚠️ WARNING: Found {count} suspicious records!{END}")
+        print(f"   - Criteria: Duration > {high_duration_limit}s OR Data > {high_data_limit}MB")
+
+        output_file = "suspicious_report.csv"
+        suspicious_df.to_csv(output_file, index=False)
+
+        print(f"{GREEN}   -> Detailed report saved to '{output_file}'{END}")
+        print(f"\n{ITALIC}Top 5 Suspicious Transactions:{END}")
+        print(suspicious_df.head(5))
+    else:
+        print(f"{GREEN}✅ No suspicious activity detected.{END}")
+
+
+if __name__ == "__main__":
+    print(f'\n{RED}--- START PROGRAM ---{END}\n')
+    print(f"Processing File: {InputFile}...")
+    start_time = time.time()
+
+    try:
+        raw_data = load_data(InputFile)
+        clean_dataframe = clean_data(raw_data)
+        analyze_data(clean_dataframe)
+        detect_fraud(clean_dataframe)
+
+        end_time = time.time()
+        print(f"\n⏱️{GREEN} Total Execution Time: {end_time - start_time:.4f} seconds.{END}")
+
+    except Exception as e:
+        print(f"\n❌{RED} Critical Error: {e}{END}")
